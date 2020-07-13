0f324639af56e6623348f1c92d8e46406812d11b
OrsoLeo <pyharef@gmail.com>
2020-07-13 12:36:44 +0300

    Commit_redaction

--- SimpleWpfApp/ViewModel/EditViewModel.cs
+++ SimpleWpfApp/ViewModel/EditViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using CommandLibrary;
 using SimpleWpfApp.Model;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
