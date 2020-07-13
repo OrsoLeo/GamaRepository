72831261a2f34593f39acebdfa8d94257d6dbf44
OrsoLeo <pyharef@gmail.com>
2020-07-13 12:09:01 +0300

    CommandCommit

---
+++ CommandLibrary/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CommandLibrary
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Predicate<object> _canExecute;
+        readonly Action<object> _execute;
+        public RelayCommand(Action<object> _ex, Predicate<object> _canEx = null)
+        {
+            _execute = _ex;
+            _canExecute = _canEx;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
